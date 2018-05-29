18f1e70dc8428f40ddf5fc0c4057d85542c823d5
LaYt <laytya@gmail.com>
2018-05-29 13:34:32 +0700

    Fixed some reports
    - fixed #15 , #16, #17

--- vQueueMessages.lua
+++ vQueueMessages.lua
@@ -233,7 +233,7 @@ CATARGS["mara"] = {
 	"maru"
 }
 CATARGS["st"] = {
-	"st",
+--	"st",  -- "Server Time" workaround... #16
 	"sunken",
 	"sunk",
 	"sunkentemple"
@@ -285,7 +285,7 @@ CATARGS["ony"] = {
 }
 CATARGS["zg"] = {
 	"zg",
-	"zul",
+	"zul gurub", -- #15
 	"gurub",
 	"hakkar"
 }
